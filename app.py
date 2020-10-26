a30d7653ef5fd123afe991c223e2407e6e98a917
swami0440 <bhavsarpratik1994@gmail.com>
2020-10-26 20:58:49 +0530

    Jinja Template inheritance added

--- app.py
+++ app.py
@@ -8,8 +8,34 @@ CORS(app)
 
 @app.route('/')
 @cross_origin()
-def hello_world():
+def home():
     return render_template("index.html")
 
+
+@app.route('/about')
+@cross_origin()
+def about():
+    return render_template("about.html")
+
+
+@app.route('/blog-list')
+@cross_origin()
+def blog_list():
+    return render_template("blog-list.html")
+
+
+@app.route('/blog')
+@cross_origin()
+def blog():
+    return render_template("blog-post.html")
+
+
+@app.route('/portfolio')
+@cross_origin()
+def portfolio():
+    return render_template("portfolio.html")
+
+
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    #app.run(port=8000, debug=True)
+    app.run(debug=True)
