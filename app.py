365a9f65eb738819bbc4ed780a1c1505f6f03c6b
swami0440 <bhavsarpratik1994@gmail.com>
2020-10-30 10:49:17 +0530

    added contact form with DB

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask, request, jsonify, render_template
 import os
 from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
 CORS(app)
 
+
+class Contacts(db.Model):
+    '''
+    sno, name phone_num, msg, date, email
+    '''
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
 
 @app.route('/')
 @cross_origin()
@@ -36,6 +54,25 @@ def portfolio():
     return render_template("portfolio.html")
 
 
+@app.route('/contact', methods = ['GET', 'POST'])
+@cross_origin()
+def contact():
+    if request.method == 'POST':
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('msg')
+        print(name,phone,email,message)
+        print(type(name),type(phone),type(email),type(message))
+        entry = Contacts(name=name, email=email, phone_num=phone, date=datetime.now(), msg=message )
+        #entry = ('pratik','fdfd@dfd.d','88888','fdfdf','dfdfdfdf')
+        db.session.add(entry)
+        db.session.commit()
+    return render_template("contact.html")
+
+
 if __name__ == '__main__':
     #app.run(port=8000, debug=True)
+    db.init_app(app)
     app.run(debug=True)
---
+++ testdb.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify, render_template
+import os
+from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+    '''
+    sno, name phone_num, msg, date, email
+    '''
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
+db.create_all()
